1d7e5c851c81dfb7de66380d68c0349c80f91c9a
bibek <SQLDEVELOPER@mediprocomputers.com>
2025-12-05 14:14:33 +0545

    adjusted local css for the development

--- greenloan/settings.py
+++ greenloan/settings.py
@@ -45,6 +45,10 @@ INSTALLED_APPS = [
 
     #custom app installed
     'core',
+
+    #third party apps for css
+    'crispy_bootstrap5',
+    'crispy_forms',
 ]
 
 MIDDLEWARE = [
@@ -129,7 +133,10 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/6.0/howto/static-files/
 
-STATIC_URL = '/static/'
+STATIC_URL = 'static/'
+STATICFILES_DIRS = [
+    BASE_DIR / "static",
+]
 STATIC_ROOT = BASE_DIR / "staticfiles"
 
 STATICFILES_STORAGE = "whitenoise.storage.CompressedManifestStaticFilesStorage"
